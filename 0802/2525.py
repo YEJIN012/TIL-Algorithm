6f8b71525ca9f524dcd9b7a2eee83a06a441bc3f
이예진 <yjlee66096@gmail.com>
2022-08-03 17:21:07 +0900

    0802

---
+++ 0802/2525.py
@@ -0,0 +1,8 @@
+now = list(map(int, input().split()))
+cooking = int(input())
+now[0] += (now[1] + cooking) // 60 
+now[0] = now[0] % 24
+now[1] = (now[1] + cooking) % 60
+print(now)
+end_time = ' '.join(map(str,now))
+print(end_time)
