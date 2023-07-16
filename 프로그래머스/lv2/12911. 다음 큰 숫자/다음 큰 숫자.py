57ef87649164c14578c93c8dc9080babc68e2b02
YEJIN LEE <109276824+YEJIN012@users.noreply.github.com>
2023-07-16 10:49:11 +0900

    Update 다음 큰 숫자.py

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,4 +1,10 @@
 def solution(n):
+    
+# 다른 진수의 문자열로 변환
+# b = format(test_value, 'b')			# 10 to 2
+# o = format(test_value, 'o')			# 10 to 8
+# h = format(test_value, 'x')			# 10 t0 16
+    
     b = format(n, 'b')
     k = 0
     for bb in b :
