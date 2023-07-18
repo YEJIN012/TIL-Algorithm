ccf13d7896f414fd63b7c49080b42faafca6f670
YEJIN LEE <109276824+YEJIN012@users.noreply.github.com>
2023-07-18 23:32:14 +0900

    Update JadenCase 문자열 만들기.py

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,13 +1,31 @@
+# def solution(s):
+#     s = s.lower()
+#     answer = ''
+#     idx0 = 1
+#     for spell in s :
+#         if idx0 == 1 and spell != ' ':  
+#             spell = spell.upper()
+#             idx0 = 0
+#         elif spell == ' ' :
+#             idx0 = 1
+#         answer += spell
+    
+#     return answer
+
 def solution(s):
     s = s.lower()
     answer = ''
     idx0 = 1
     for spell in s :
-        if idx0 == 1 and spell != ' ':
+        # if - elif 순서 중요!
+        # idx0 == 1을 먼저 if로 확인하면, 문자열이 공백으로 시작하는 경우 첫단어를 대문자로 변환하지못함.
+        # ex) " aaa aa" -> " Aaa Aa"
+        if spell == ' ' :
+            idx0 = 1
+        elif idx0 == 1 :
             spell = spell.upper()
             idx0 = 0
-        elif spell == ' ' :
-            idx0 = 1
+        
         answer += spell
     
     return answer
