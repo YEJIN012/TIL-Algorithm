6c0e7629eb052f092ae1b52cae6f6736f8d07d64
YEJIN LEE <109276824+YEJIN012@users.noreply.github.com>
2023-08-01 23:21:52 +0900

    Update 숫자의 표현.py

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,12 +1,12 @@
 def solution(n):
     nums = list(range(1, n+1))
     cnt = 0
-    for i in range(n-1, -1, -1) :
+    # 1부터 n까지 모든 부분집합의 합을 구한다.
+    for i in range(n) :
         for j in range(i+1, n+1) :
-            # print(nums[i:j])
-            if sum(nums[i:j]) == n :
+            if sum(nums[i:j]) == n :    # 합이 n이 되면 cnt +1 후, 중단.
                 cnt += 1
                 break
-            elif sum(nums[i:j]) > n :
+            elif sum(nums[i:j]) > n :   # 합이 n을 이미 넘으면 중단.
                 break
     return cnt
