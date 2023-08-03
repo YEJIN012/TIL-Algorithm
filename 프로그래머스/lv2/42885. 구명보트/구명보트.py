c47fdc482acb1a31266b1793b2d639f1014e49e6
YEJIN LEE <109276824+YEJIN012@users.noreply.github.com>
2023-08-03 11:17:47 +0900

    Update 구명보트.py

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,7 +1,10 @@
 # 최대 2명이니까 혼자 타거나 둘이 타거나.
 # 최대한 무게를 채워야하므로, 가장 적은 무게와 가장 큰 무게를 같이 태운다.
 # 가장 적은 무게와 가장 큰 무게 > limit 이라면 큰 무게 사람 먼저 혼자 태운다.
+
+# deque 써야 효율성 통과
 from collections import deque
+
 def solution(people, limit):
     people = deque(sorted(people, reverse=True))
     boat = 0
