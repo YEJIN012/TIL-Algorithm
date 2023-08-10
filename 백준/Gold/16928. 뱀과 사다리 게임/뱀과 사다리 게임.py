b2f9d906311121c887342c03092d831e8e7b14dc
YEJIN LEE <109276824+YEJIN012@users.noreply.github.com>
2023-08-10 20:29:06 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,27 @@
+N, M = map(int,input().split())
+arr = [0] * 101
+for _ in range(N+M) :
+    x, y = map(int,input().split())
+    arr[x] = y
+
+def bfs(n) :
+    q = [n]
+    visited = [0] * 101
+    while q :
+        i = q.pop(0)
+
+        for di in [i for i in range(1, 7)] :
+            ni = i + di
+            if 1<=ni<=100 and visited[ni] == 0 :
+                if arr[ni] == 0 :   # 뱀과 사다리가 없으면,
+                    visited[ni] = visited[i] + 1  # 방문 표시 = 주사위 굴린 횟수 로 생각하고 1씩 증가해가며 표시.
+                    q.append(ni)                  # ni만 q에 담기
+                else :              # 있으면,  # 무조건 이동하므로, ni에서 다시 시작할 수 없다. 따라서 ni는 방문표시와 q를 담지 않는다.
+                    if visited[arr[ni]] == 0 :
+                        visited[arr[ni]] = visited[i] + 1 # 뱀 사다리 이동 위치의 뱀사다리도 확인해야하므로, 뱀 사다리로 이동한 위치는 방문표시를 하지 않는다.
+                        q.append(arr[ni])               # 뱀 사다리 이동 위치만 q에 담기.
+
+            if ni == 100 :
+                return visited[ni]
+
+print(bfs(1))
